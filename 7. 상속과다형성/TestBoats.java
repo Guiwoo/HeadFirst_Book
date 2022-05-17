2a098c57809583a9c12f8f1f8f8fe2e40992185c
Guiwoo <park.guiwoo@hotmail.com>
2022-05-17 23:02:54 +0900

    7.상속과 다형성

---
+++ 7. 상속과다형성/TestBoats.java
@@ -0,0 +1,37 @@
+class TestBoats{
+  public static void main(String[] args){
+    Boat b1 = new Boat();
+    Sailboat b2 = new Sailboat();
+    Rowboat b3 = new Rowboat();
+
+    b2.setLength(32);
+    b1.move();
+    b3.move();
+    b2.move();
+  }
+}
+
+class Boat{
+  private int length;
+  public void setLength(int len){
+    length = len;
+  }
+  public int getLength(){
+    return length;
+  }
+  public void move(){
+    System.out.print("drift ");
+  }
+}
+
+class Sailboat extends Boat{
+  public void move(){
+    System.out.print("Hoist sail");
+  }
+}
+
+class Rowboat extends Boat{
+  public void rowTheBoat(){
+    System.out.println("stroke natasha");
+  }
+}
